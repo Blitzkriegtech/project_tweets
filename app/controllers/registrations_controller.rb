0e3f1400a19c0a8338e529f8cff109f7a3441580
Aljun D. Rendon <aljunrendon@yahoo.com>
2025-06-23 13:38:18 +0800

    add user session cookie

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # session[:user_id] = @user.id
+      session[:user_id] = @user.id # encryted cookie to track user session wheter they logged in
       redirect_to root_path, notice: "Successfully created account"
     else
       render :new
